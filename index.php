64871b607081ed809fae5505654d287021546fea
devaaprian <theprojessa@gmail.com>
2020-06-08 13:55:02 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+        @font-face {
+            font-family: 'courigette';
+            src: url('font/BubbleBobble-rg3rx.ttf') format('truetype');
+        }
+
+        .caption {
+            position: absolute;
+            top: 28%;
+            right: 42%;
+            font-size: 100px;
+            font-family: 'courigette';
+            color: snow;
+        }
+
+        .btn-grad {
+            background-image: linear-gradient(to right, #1FA2FF 0%, #12D8FA 51%, #1FA2FF 100%);
+            outline: none;
+            width: 100%;
+            height: 50px;
+            margin: auto;
+            border-radius: 40px;
+            border: none;
+            font-family: 'courigette';
+            color: white;
+            font-size: 18px;
+        }
+
+        .btn-grad:hover {
+            background-position: right center;
+        }
+    </style>
+</head>
+
+<body>
+
+
+    <div class="container-fluid" style="padding: 0; margin:0;">
+
+        <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel" style="opacity: 0.8">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-interval="10000">
+                    <img src="img/2.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-interval="2000">
+                    <img src="img/3.jpg" class="d-block w-100" alt="..." style="max-height: 598px;">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/4.jpg" class="d-block w-100" alt="..." style="max-height: 598px;">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+        <p class="caption">KelasKu</p>
+    </div>
+
+    <div class="container-md shadow p-3 mb-5 centered  p-5" style="padding: 0; margin-top:0;border-radius:15px;position:relative;">
+        <img class="rounded-circle" src="img/profile.jpg" alt="" style="max-width: 200px;max-height:200px;position:absolute;top:-100px;right:40%;">
+        <br>
+        <div class="row row-cols-4 mt-5" style="margin-top: 50px">
+            <div class="col">
+                <div class="card shadow p-3" style="padding-top: 5px;border-radius:15px;">
+                    <img src="img/vek1.jpg" class="card-img-top centered" alt="...">
+                    <hr>
+                    <div class="card-body">
+                        <button class="btn-grad">Profile</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow p-3" style="padding-top: 5px;border-radius:15px;">
+                    <img src="img/vek2.jpg" class="card-img-top centered" alt="..." style="padding-top: 10px;">
+                    <hr>
+                    <div class="card-body">
+                        <button class="btn-grad">Galery</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow p-3" style="padding-top: 5px;border-radius:15px;">
+                    <img src="img/vek3.jpg" class="card-img-top centered" alt="...">
+                    <hr>
+                    <div class="card-body">
+                        <button class="btn-grad">Article</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow p-3" style="padding-top: 5px;border-radius:15px;">
+                    <img src="img/vek4.jpg" class="card-img-top centered" alt="...">
+                    <hr>
+                    <div class="card-body">
+                        <button class="btn-grad">Announcement</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+        <!-- Footer Elements -->
+        <div class="container">
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-lg-2 col-md-12 mb-4">
+
+                    <!--Image-->
+                    <div class="view overlay z-depth-1-half">
+                        <img src="img/fotbar1.jpg" class="img-fluid" alt="">
+                        <a href="">
+                            <div class="mask rgba-white-light"></div>
+                        </a>
+                    </div>
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-2 col-md-6 mb-4">
+
+                    <!--Image-->
+                    <div class="view overlay z-depth-1-half">
+                        <img src="img/fotbar3.jpg" class="img-fluid" alt="">
+                        <a href="">
+                            <div class="mask rgba-white-light"></div>
+                        </a>
+                    </div>
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-2 col-md-6 mb-4">
+
+                    <!--Image-->
+                    <div class="view overlay z-depth-1-half">
+                        <img src="img/fotbar5.jpg" class="img-fluid" alt="">
+                        <a href="">
+                            <div class="mask rgba-white-light"></div>
+                        </a>
+                    </div>
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-2 col-md-12 mb-4">
+
+                    <!--Image-->
+                    <div class="view overlay z-depth-1-half">
+                        <img src="img/fotbar4.jpg" class="img-fluid" alt="">
+                        <a href="">
+                            <div class="mask rgba-white-light"></div>
+                        </a>
+                    </div>
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-2 col-md-6 mb-4">
+
+                    <!--Image-->
+                    <div class="view overlay z-depth-1-half">
+                        <img src="img/fotbar5.jpg" class="img-fluid" alt="">
+                        <a href="">
+                            <div class="mask rgba-white-light"></div>
+                        </a>
+                    </div>
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-2 col-md-6 mb-4">
+
+                    <!--Image-->
+                    <div class="view overlay z-depth-1-half">
+                        <img src="img/fotbar6.jpg" class="img-fluid" alt="">
+                        <a href="">
+                            <div class="mask rgba-white-light"></div>
+                        </a>
+                    </div>
+
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+        </div>
+        <!-- Footer Elements -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="#"> Deva Apriana</a>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="bootstrap/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
